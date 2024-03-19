8175988d4d976eb3734d6f80bada6b01adc50633
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-03-19 11:15:43 +0330

    add ground animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 const dino = document.querySelector("[data-dino]");
+const startMSG = document.querySelector(".start-dialog");
+const scoreContainer = document.querySelector(".score");
+
+let playing = false;
+let score = 0;
+
+function init() {
+  playing = false;
+  score = 0;
+  startMSG.classList.remove("hidden");
+  scoreContainer.textContent = 0;
+}
 
 document.addEventListener("keydown", function (e) {
-  if (e.code === "Space" && !dino.classList.contains("jump-animation")) {
+  if (
+    playing &&
+    e.code === "Space" &&
+    !dino.classList.contains("jump-animation")
+  ) {
     dino.classList.add("jump-animation");
 
     setTimeout(() => {
