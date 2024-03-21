95768793cf4c6879bffe87138db7ea1cc446ff8b
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-03-21 13:59:17 +0330

    create cactus & random cactus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const game = document.querySelector(".game");
 const dino = document.querySelector("[data-dino]");
 const startMSG = document.querySelector(".start-dialog");
 const scoreElement = document.querySelector(".score");
@@ -6,6 +7,8 @@ const ground = document.querySelector(".ground");
 let playing = false;
 let score = 0;
 
+init();
+
 function init() {
   playing = false;
   score = 0;
@@ -13,9 +16,9 @@ function init() {
   scoreElement.textContent = score;
   ground.classList.remove("groundAnimation");
   dino.src = "../img/dino-stationary.png";
+
+  document.addEventListener("keydown", gameStart);
 }
-init();
-if (!playing) document.addEventListener("keydown", gameStart);
 
 function gameStart() {
   playing = true;
@@ -44,3 +47,25 @@ document.addEventListener("keydown", function (e) {
   }
   if (playing && e.code === "Escape") init();
 });
+
+function createCactus() {
+  if (playing) {
+    const cactus = document.createElement("div");
+    cactus.classList.add("cactus");
+    game.appendChild(cactus);
+
+    setTimeout(() => {
+      game.removeChild(cactus);
+    }, 2000);
+  }
+}
+
+function randomCactus(minTime, maxTime) {
+  if (playing) {
+    const delay = Math.random() * (maxTime - minTime) + minTime;
+    // console.log(Number(delay));
+    setTimeout(createCactus, delay * 1000);
+  }
+}
+
+setInterval(() => randomCactus(0, 2), 1500);
